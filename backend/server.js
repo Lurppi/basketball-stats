f9ed0af2d828f30c510e27419ba0cc652136d0ca
Dennis <dennis.uhlig@icloud.com>
2024-08-28 04:19:44 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,31 @@
-// backend/server.js
 const express = require('express');
 const cors = require('cors'); 
 const app = express();
 const playersRoutes = require('./routes/players');
-const teamsRoutes = require('./routes/teams'); // Hinzufügen der teamsRoutes
-const homeRoutes = require('./routes/home'); // Hinzufügen der homeRoutes
+const teamsRoutes = require('./routes/teams');
+const homeRoutes = require('./routes/home');
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/api/players', playersRoutes);
-app.use('/api/teams', teamsRoutes); // Registrieren der teamsRoutes
-app.use('/api/home', homeRoutes); // Registrieren der homeRoutes
 
+// Routes
+app.use('/api/players', playersRoutes);
+app.use('/api/teams', teamsRoutes);
+app.use('/api/home', homeRoutes);
+
+// Default route for root path
+app.get('/', (req, res) => {
+  res.send('Welcome to the Basketball Stats API');
+});
+
+// Error handling for 404
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
