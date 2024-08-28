f4a7a8fa28e85c5eab9f101125e827c57a85c245
Dennis <dennis.uhlig@icloud.com>
2024-08-28 21:14:58 +0200

    update vite.config.jsx und vercel.json

--- frontend/vite.config.jsx
+++ frontend/vite.config.jsx
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    open: true,
   },
 });
