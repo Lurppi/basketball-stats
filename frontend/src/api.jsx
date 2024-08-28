78dec89c115e4c8472151971732dea13b837ee5e
Dennis <dennis.uhlig@icloud.com>
2024-08-28 19:59:55 +0200

    update api

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,29 +1,17 @@
-const API_BASE_URL = "https://backend-sandy-rho.vercel.app/api";
-
-export const fetchPlayersData = async () => {
-  try {
-    const response = await fetch(`${API_BASE_URL}/players/Regular/Totals`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Fetch players data failed:", error);
-    return [];
+export const fetchPlayers = async (league, statsType) => {
+  const response = await fetch(`https://backend-sandy-rho.vercel.app/api/players/${league}/${statsType}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
   }
+  const data = await response.json();
+  return data;
 };
 
-export const fetchTeamsData = async () => {
-  try {
-    const response = await fetch(`${API_BASE_URL}/teams/Regular/Totals`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Fetch teams data failed:", error);
-    return [];
+export const fetchTeams = async (league, statsType) => {
+  const response = await fetch(`https://backend-sandy-rho.vercel.app/api/teams/${league}/${statsType}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
   }
+  const data = await response.json();
+  return data;
 };
