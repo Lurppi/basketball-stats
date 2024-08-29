8e6a7e41b1ba5fc2a5320c57961ebf6046878fae
Dennis <dennis.uhlig@icloud.com>
2024-08-29 08:06:31 +0200

    update

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,17 +1,19 @@
-export const fetchPlayers = async (league, statsType) => {
-  const response = await fetch(`https://backend-sandy-rho.vercel.app/api/players/${league}/${statsType}`);
+const BASE_URL = 'https://backend-sandy-rho.vercel.app/api';
+
+const handleFetchResponse = async (response) => {
   if (!response.ok) {
-    throw new Error('Network response was not ok');
+    const errorMessage = await response.text();
+    throw new Error(`Network response was not ok: ${errorMessage}`);
   }
-  const data = await response.json();
-  return data;
+  return response.json();
+};
+
+export const fetchPlayers = async (league, statsType) => {
+  const response = await fetch(`${BASE_URL}/players/${league}/${statsType}`);
+  return handleFetchResponse(response);
 };
 
 export const fetchTeams = async (league, statsType) => {
-  const response = await fetch(`https://backend-sandy-rho.vercel.app/api/teams/${league}/${statsType}`);
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  const data = await response.json();
-  return data;
+  const response = await fetch(`${BASE_URL}/teams/${league}/${statsType}`);
+  return handleFetchResponse(response);
 };
