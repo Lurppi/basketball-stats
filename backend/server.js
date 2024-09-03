78f21fc8c1d9643698154f50f662ff423bfb2cb9
Dennis <dennis.uhlig@icloud.com>
2024-09-03 18:54:27 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,14 @@ const corsOptions = {
     'https://frontend-iota-seven-93.vercel.app',
     'https://frontend-lurppis-projects.vercel.app'
   ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // Erlaubte Methoden
-  allowedHeaders: ['Content-Type', 'Authorization'], // Erlaubte Header
   optionsSuccessStatus: 200, // Für legacy browser support
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Erlaubte Methoden
+  allowedHeaders: 'Content-Type, Authorization', // Erlaubte Header
 };
 
 // Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
-
-// Handle preflight requests for all routes
-app.options('*', cors(corsOptions));  // <- Dies stellt sicher, dass Preflight-Anfragen korrekt beantwortet werden
 
 // Routes
 app.use('/api/players', playersRoutes);
@@ -40,12 +37,6 @@ app.get('/', (req, res) => {
 // Error handling for 404
 app.use((req, res) => {
   res.status(404).send('Not Found');
-});
-
-// Global error handling middleware
-app.use((err, req, res, next) => {
-  console.error('Internal Server Error:', err.stack);
-  res.status(500).send('Internal Server Error');
 });
 
 // Start server
