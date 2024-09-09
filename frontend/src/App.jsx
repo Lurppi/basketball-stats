1998e8668d34e395796974d1d1b266766c22bcb7
Dennis <dennis.uhlig@icloud.com>
2024-09-09 09:53:02 +0200

    1111

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,23 @@ import Home from './assets/Home';
 import Players from './assets/Players';
 import Teams from './assets/Teams';
 import Impressum from './assets/Impressum';
-import Glossary from './assets/Glossary'; 
-import Footer from './assets/Footer';
+import Glossary from './assets/Glossary';
 import Policy from './assets/Policy';
-import Header from './assets/Header'; // Header importieren
-import './assets/Header.css'; // Header CSS importieren
-
+import Header from './assets/Header';
+import './assets/Header.css';
 
 function App() {
   return (
     <Router>
-        <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/players" element={<Players />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/impressum" element={<Impressum />} />
-        <Route path="/glossary" element={<Glossary />} />  
-        <Route path="/privacy-policy" element={<Policy />} />  
+        <Route path="/glossary" element={<Glossary />} />
+        <Route path="/privacy-policy" element={<Policy />} />
       </Routes>
-     </Router>
+    </Router>
   );
 }
 
