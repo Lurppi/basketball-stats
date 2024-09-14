5156ee39f0a26276d67f282c583c3c9f6092bf75
Dennis <dennis.uhlig@icloud.com>
2024-09-14 07:03:38 +0200

    111

--- frontend/src/assets/Navbar.jsx
+++ frontend/src/assets/Navbar.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <Link to="/">HOME</Link>
-      <Link to="/standings">STANDINGS</Link>
-      
-      {/* Dropdown for Teams */}
-      <div className="dropdown">
-        <button className="dropbtn">TEAMS</button>
-        <div className="dropdown-content">
-          <Link to="/teams">DASHBOARD</Link>
-          <Link to="/teams/form">FORM</Link>
+      <div className="container nav-container">
+        <input
+          type="checkbox"
+          id="menu-toggle"
+          className="checkbox"
+          checked={isOpen}
+          onChange={handleToggle}
+        />
+        <div className="hamburger-lines" onClick={handleToggle}>
+          <span className="line line1"></span>
+          <span className="line line2"></span>
+          <span className="line line3"></span>
+        </div>
+
+        <div className="logo">
+         </div>
+
+        <div className={`menu-items ${isOpen ? 'open' : ''}`}>
+          <Link to="/">HOME</Link>
+          <Link to="/standings">STANDINGS</Link>
+
+          <div className="dropdown">
+            <button className="dropbtn">TEAMS</button>
+            <div className="dropdown-content">
+              <Link to="/teams">DASHBOARD</Link>
+              <Link to="/teams/form">FORM</Link>
+            </div>
+          </div>
+
+          <Link to="/players">PLAYERS</Link>
         </div>
       </div>
-          <Link to="/players">PLAYERS</Link>
     </nav>
   );
 };
