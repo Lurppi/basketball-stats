131768bdc7fdd5a2c491338763e2d93fb0af04e2
Dennis <dennis.uhlig@icloud.com>
2024-09-18 06:15:47 +0200

    11

--- backend/controllers/playersController.js
+++ backend/controllers/playersController.js
@@ -109,7 +109,7 @@ const assignBadges = (seasonData) => {
   }
 
   // Lockdown Defender Badge
-  if (parseFloat(seasonData['SPG']) >= 1.5 && parseFloat(seasonData['ST%']) >= 2.5 && parseFloat(seasonData['STOPS_Gm']) >= 5 && parseFloat(seasonData['DRTG_ADJ']) < 85) {
+  if (parseFloat(seasonData['ST%']) >= 4.0 && parseFloat(seasonData['DRTG_ADJ']) < 90) {
     badges.push("Lockdown Defender");
     logBadge("Lockdown Defender");
   }
