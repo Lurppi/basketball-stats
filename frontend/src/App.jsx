70b88e1596caf43b796c66f6e376544cfa55ff8a
Dennis <dennis.uhlig@icloud.com>
2024-09-19 16:35:33 +0200

    11

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './assets/Home';
 import Players from './assets/Players';
 import Teams from './assets/Teams';
-import Form from './assets/Form';
+import Form from './assets/Form';  // Import der neuen Form-Komponente
 import Impressum from './assets/Impressum';
 import Glossary from './assets/Glossary';
 import Policy from './assets/Policy';
-import Rankings from './assets/Rankings';
+import Rankings from './assets/Rankings'; // Rankings-Komponente importieren
 import Header from './assets/Header';
-import PlayerPage from './assets/PlayerPage';
-import PlayersWithBadges from './assets/PlayersWithBadges'; // Neue Komponente
+import PlayerPage from './assets/PlayerPage'; // Importiere PlayerPage
+import './assets/Header.css';
 
 function App() {
   return (
@@ -20,13 +19,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/players" element={<Players />} />
-        <Route path="/players/badges" element={<PlayersWithBadges />} /> {/* Neue Route */}
         <Route path="/teams" element={<Teams />} />
-        <Route path="/teams/form" element={<Form />} />
+        <Route path="/teams/form" element={<Form />} /> {/* Neue Form-Route */}
         <Route path="/standings" element={<Rankings />} />
         <Route path="/impressum" element={<Impressum />} />
         <Route path="/glossary" element={<Glossary />} />
         <Route path="/privacy-policy" element={<Policy />} />
+
+        {/* Neue Route für dynamische Spielerprofile */}
         <Route path="/player/:id" element={<PlayerPage />} />
       </Routes>
     </Router>
