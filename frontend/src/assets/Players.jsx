3d1231be9eb8f9c4c72d58ea821b2390253e7a00
Dennis <dennis.uhlig@icloud.com>
2024-09-26 23:30:54 +0200

    11

--- frontend/src/assets/Players.jsx
+++ frontend/src/assets/Players.jsx
@@ -53,21 +53,29 @@ const Players = () => {
   const [allPlayers, setAllPlayers] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [headers, setHeaders] = useState([]);
-  const [filters, setFilters] = useState({
-    season: '',
-    league: '', 
-    statsType: 'Advanced 1', 
-    division: '',
+  const defaultFilters = {
+    season: '20232024',
+    league: 'NBBL',
+    statsType: 'Advanced 1',
+    division: 'NBBL A',
     team: 'All',
     position: 'All',
     offensiveRole: 'All',
-    seasonType: '', 
+    seasonType: 'PLAYOFFS',
     born: 'All',
     gamesPlayed: '',
     minutesPlayed: '',
     sortStat: 'MP',
     sortDirection: 'desc',
-  });
+  };
+
+  // Lade Filtereinstellungen aus SessionStorage oder nutze Standardwerte
+  const initialFilters = () => {
+    const savedFilters = JSON.parse(sessionStorage.getItem('filters'));
+    return savedFilters || defaultFilters;
+  };
+
+  const [filters, setFilters] = useState(() => initialFilters());
 
   const [seasons, setSeasons] = useState([]);
   const [leagues, setLeagues] = useState([]);
@@ -83,12 +91,154 @@ const Players = () => {
   const rowsPerPage = 20;
   const [currentPage, setCurrentPage] = useState(1);
 
+  // Speichere die Filtereinstellungen in SessionStorage, sobald sie sich ändern
+  useEffect(() => {
+    sessionStorage.setItem('filters', JSON.stringify(filters));
+  }, [filters]);
+
+  const updateDropdownValues = () => {
+    const filtered = applyFilters(allPlayers);
+
+    // Aktualisiere Leagues und prüfe den aktuellen Wert
+    const uniqueLeagues = [...new Set(
+      allPlayers
+        .filter(player => player[headers.indexOf('SEASON_YEAR')] === filters.season)
+        .map(player => player[headers.indexOf('LEAGUE')])
+    )];
+    setLeagues(uniqueLeagues);
+
+    if (!uniqueLeagues.includes(filters.league)) {
+      setFilters(prev => ({
+        ...prev,
+        league: uniqueLeagues[0],
+        division: '', // Setze zurück, um sicherzustellen, dass ein neuer Division-Wert gewählt wird
+        seasonType: '', // Setze zurück, um einen neuen Season-Type-Wert zu wählen
+        team: 'All', // Setze das Team auf "All" zurück
+      }));
+    }
+
+    // Aktualisiere Divisions und prüfe den aktuellen Wert
+    const uniqueDivisions = [...new Set(
+      allPlayers
+        .filter(player =>
+          player[headers.indexOf('SEASON_YEAR')] === filters.season &&
+          player[headers.indexOf('LEAGUE')] === filters.league
+        )
+        .map(player => player[headers.indexOf('DIV')])
+    )];
+    setDivisions(uniqueDivisions);
+
+    if (!uniqueDivisions.includes(filters.division)) {
+      setFilters(prev => ({
+        ...prev,
+        division: uniqueDivisions[0] || '' // Fallback auf den ersten gültigen Wert
+      }));
+    }
+
+    // Aktualisiere Season Types und prüfe den aktuellen Wert
+    const uniqueSeasonTypes = [...new Set(
+      allPlayers
+        .filter(player =>
+          player[headers.indexOf('SEASON_YEAR')] === filters.season &&
+          player[headers.indexOf('LEAGUE')] === filters.league &&
+          player[headers.indexOf('DIV')] === filters.division
+        )
+        .map(player => player[headers.indexOf('SEASON_TYPE')])
+    )];
+    setSeasonTypes(uniqueSeasonTypes);
+
+    if (!uniqueSeasonTypes.includes(filters.seasonType)) {
+      setFilters(prev => ({
+        ...prev,
+        seasonType: uniqueSeasonTypes[0] || '' // Fallback auf den ersten gültigen Wert
+      }));
+    }
+
+    // Aktualisiere Teams und sortiere alphabetisch, "All" bleibt an erster Stelle
+    let uniqueTeams = ['All', ...new Set(
+      allPlayers
+        .filter(player =>
+          player[headers.indexOf('SEASON_YEAR')] === filters.season &&
+          player[headers.indexOf('LEAGUE')] === filters.league &&
+          player[headers.indexOf('DIV')] === filters.division &&
+          player[headers.indexOf('SEASON_TYPE')] === filters.seasonType
+        )
+        .map(player => player[headers.indexOf('TEAM')])
+    )].sort();
+
+    if (uniqueTeams.includes('All')) {
+      uniqueTeams = ['All', ...uniqueTeams.filter(team => team !== 'All')];
+    }
+    setTeams(uniqueTeams);
+
+    if (!uniqueTeams.includes(filters.team)) {
+      setFilters(prev => ({
+        ...prev,
+        team: uniqueTeams[0] || 'All' // Fallback auf den ersten gültigen Wert
+      }));
+    }
+
+    // Aktualisiere Positions und sortiere alphabetisch, "All" bleibt an erster Stelle
+    let uniquePositions = ['All', ...new Set(
+      allPlayers
+        .filter(player =>
+          player[headers.indexOf('SEASON_YEAR')] === filters.season &&
+          player[headers.indexOf('LEAGUE')] === filters.league &&
+          player[headers.indexOf('DIV')] === filters.division &&
+          player[headers.indexOf('SEASON_TYPE')] === filters.seasonType &&
+          (filters.team === 'All' || player[headers.indexOf('TEAM')] === filters.team)
+        )
+        .map(player => player[headers.indexOf('POS')])
+    )].sort();  // Sortiere alphabetisch
+
+    // Sicherstellen, dass "All" ganz oben bleibt
+    if (uniquePositions.includes('All')) {
+      uniquePositions = ['All', ...uniquePositions.filter(pos => pos !== 'All')];
+    }
+    setPositions(uniquePositions);
+
+    const uniqueOffensiveRoles = ['All', ...new Set(
+      allPlayers
+        .filter(player =>
+          player[headers.indexOf('SEASON_YEAR')] === filters.season &&
+          player[headers.indexOf('LEAGUE')] === filters.league &&
+          player[headers.indexOf('DIV')] === filters.division &&
+          player[headers.indexOf('SEASON_TYPE')] === filters.seasonType &&
+          (filters.team === 'All' || player[headers.indexOf('TEAM')] === filters.team)
+        )
+        .map(player => player[headers.indexOf('ROLE')])
+    )];
+    setOffensiveRoles(uniqueOffensiveRoles);
+
+    const uniqueBornYears = ['All', ...new Set(
+      allPlayers
+        .filter(player =>
+          player[headers.indexOf('SEASON_YEAR')] === filters.season &&
+          player[headers.indexOf('LEAGUE')] === filters.league &&
+          player[headers.indexOf('DIV')] === filters.division &&
+          player[headers.indexOf('SEASON_TYPE')] === filters.seasonType &&
+          (filters.team === 'All' || player[headers.indexOf('TEAM')] === filters.team)
+        )
+        .map(player => player[headers.indexOf('BORN')])
+    )].sort((a, b) => {
+      if (a === 'All') return -1; // 'All' bleibt immer als erster Eintrag
+      return a - b; // Numerische Sortierung der Jahre
+    });
+    setBornYears(uniqueBornYears);
+  };
+
+  // Verwende den `useEffect`, um die Funktion aufzurufen, wenn ein übergeordneter Filter geändert wird
+  useEffect(() => {
+    if (allPlayers.length > 0) {
+      updateDropdownValues();  // Aktualisiere Dropdown-Werte nur, wenn Daten verfügbar sind
+    }
+  }, [filters.season, filters.league, filters.division, filters.seasonType, allPlayers, headers]);
+
+  // Daten laden und verarbeiten
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Lade die Daten basierend auf statsType und season
         const data = await fetchPlayers(filters.statsType, filters.season);
-
         if (data && data.length > 0) {
           const selectedColumns = columnMappings[filters.statsType];
           setHeaders(selectedColumns);
@@ -101,19 +251,25 @@ const Players = () => {
           setFilteredData(processedData);
 
           // Dynamische Ermittlung der verfügbaren Seasons
-          const availableSeasons = [...new Set(data.map(player => player['SEASON_YEAR']))].sort(); // Sortiere die Seasons
-
-          // Speichere alle verfügbaren Seasons
+          const availableSeasons = [...new Set(data.map(player => player['SEASON_YEAR']))].sort();
           setSeasons(availableSeasons);
 
-          // Setze die neueste Saison als Standard, ohne das Array zu verändern
           const latestSeason = availableSeasons[availableSeasons.length - 1];
 
-          // Setze die Season, falls noch nicht gesetzt
-          setFilters(prevFilters => ({
-            ...prevFilters,
-            season: prevFilters.season || latestSeason // Setze die neueste verfügbare Saison, falls nicht gesetzt
-          }));
+          if (!filters.season) {
+            setFilters(prevFilters => ({
+              ...prevFilters,
+              season: prevFilters.season || latestSeason
+            }));
+          }
+
+          // Lade die Filter aus dem SessionStorage
+          setTimeout(() => {
+            const savedFilters = JSON.parse(sessionStorage.getItem('filters'));
+            if (savedFilters) {
+              setFilters(savedFilters);
+            }
+          }, 0);
         }
 
         setLoading(false);
@@ -155,144 +311,7 @@ const Players = () => {
       );
     });
   };
-
-  useEffect(() => {
-    const updateDropdownValues = () => {
-      const filtered = applyFilters(allPlayers);
-
-      // Aktualisiere Leagues und prüfe den aktuellen Wert
-      const uniqueLeagues = [...new Set(
-        allPlayers
-          .filter(player => player[headers.indexOf('SEASON_YEAR')] === filters.season)
-          .map(player => player[headers.indexOf('LEAGUE')])
-      )];
-      setLeagues(prev => prev.length !== uniqueLeagues.length ? uniqueLeagues : prev);
-
-      if (!uniqueLeagues.includes(filters.league)) {
-        setFilters(prev => ({ ...prev, league: uniqueLeagues[0] }));
-      }
-
-      // Aktualisiere Divisions und prüfe den aktuellen Wert
-      const uniqueDivisions = [...new Set(
-        allPlayers
-          .filter(player =>
-            player[headers.indexOf('SEASON_YEAR')] === filters.season &&
-            player[headers.indexOf('LEAGUE')] === filters.league
-          )
-          .map(player => player[headers.indexOf('DIV')])
-      )];
-      setDivisions(prev => prev.length !== uniqueDivisions.length ? uniqueDivisions : prev);
-
-      if (!uniqueDivisions.includes(filters.division)) {
-        setFilters(prev => ({ ...prev, division: uniqueDivisions[0] }));
-      }
-
-      // Aktualisiere Season Types und prüfe den aktuellen Wert
-      const uniqueSeasonTypes = [...new Set(
-        allPlayers
-          .filter(player =>
-            player[headers.indexOf('SEASON_YEAR')] === filters.season &&
-            player[headers.indexOf('LEAGUE')] === filters.league &&
-            player[headers.indexOf('DIV')] === filters.division
-          )
-          .map(player => player[headers.indexOf('SEASON_TYPE')])
-      )];
-
-      setSeasonTypes(uniqueSeasonTypes);
-
-      // Fallback auf den ersten verfügbaren Season Type, wenn der aktuelle nicht gültig ist
-      if (!uniqueSeasonTypes.includes(filters.seasonType)) {
-        setFilters(prev => ({
-          ...prev,
-          seasonType: uniqueSeasonTypes.length > 0 ? uniqueSeasonTypes[0] : ''
-        }));
-      }
-
-      // Aktualisiere Teams und sortiere alphabetisch, "All" bleibt an erster Stelle
-      let uniqueTeams = ['All', ...new Set(
-        allPlayers
-          .filter(player =>
-            player[headers.indexOf('SEASON_YEAR')] === filters.season &&
-            player[headers.indexOf('LEAGUE')] === filters.league &&
-            player[headers.indexOf('DIV')] === filters.division &&
-            player[headers.indexOf('SEASON_TYPE')] === filters.seasonType
-          )
-          .map(player => player[headers.indexOf('TEAM')])
-      )].sort();  // Sortiere alphabetisch
-
-      // Sicherstellen, dass "All" ganz oben bleibt
-      if (uniqueTeams.includes('All')) {
-        uniqueTeams = ['All', ...uniqueTeams.filter(team => team !== 'All')];
-      }
-      setTeams(uniqueTeams);
-
-      if (filters.team !== 'All' && !uniqueTeams.includes(filters.team)) {
-        setFilters(prev => ({ ...prev, team: 'All' }));
-      }
-
-      // Aktualisiere Positions und sortiere alphabetisch, "All" bleibt an erster Stelle
-      let uniquePositions = ['All', ...new Set(
-        allPlayers
-          .filter(player =>
-            player[headers.indexOf('SEASON_YEAR')] === filters.season &&
-            player[headers.indexOf('LEAGUE')] === filters.league &&
-            player[headers.indexOf('DIV')] === filters.division &&
-            player[headers.indexOf('SEASON_TYPE')] === filters.seasonType &&
-            (filters.team === 'All' || player[headers.indexOf('TEAM')] === filters.team)
-          )
-          .map(player => player[headers.indexOf('POS')])
-      )].sort();  // Sortiere alphabetisch
-
-      // Sicherstellen, dass "All" ganz oben bleibt
-      if (uniquePositions.includes('All')) {
-        uniquePositions = ['All', ...uniquePositions.filter(pos => pos !== 'All')];
-      }
-      setPositions(uniquePositions);
-
-      const uniqueOffensiveRoles = ['All', ...new Set(
-        allPlayers
-          .filter(player =>
-            player[headers.indexOf('SEASON_YEAR')] === filters.season &&
-            player[headers.indexOf('LEAGUE')] === filters.league &&
-            player[headers.indexOf('DIV')] === filters.division &&
-            player[headers.indexOf('SEASON_TYPE')] === filters.seasonType &&
-            (filters.team === 'All' || player[headers.indexOf('TEAM')] === filters.team)
-          )
-          .map(player => player[headers.indexOf('ROLE')])
-      )];
-      setOffensiveRoles(uniqueOffensiveRoles);
-
-      const uniqueBornYears = ['All', ...new Set(
-        allPlayers
-          .filter(player =>
-            player[headers.indexOf('SEASON_YEAR')] === filters.season &&
-            player[headers.indexOf('LEAGUE')] === filters.league &&
-            player[headers.indexOf('DIV')] === filters.division &&
-            player[headers.indexOf('SEASON_TYPE')] === filters.seasonType &&
-            (filters.team === 'All' || player[headers.indexOf('TEAM')] === filters.team)
-          )
-          .map(player => player[headers.indexOf('BORN')])
-      )].sort((a, b) => {
-        if (a === 'All') return -1; // 'All' bleibt immer als erster Eintrag
-        return a - b; // Numerische Sortierung der Jahre
-      });
-      setBornYears(uniqueBornYears);
-    };
-
-    updateDropdownValues();
-  }, [
-    filters.season,
-    filters.league,
-    filters.division,
-    filters.seasonType,
-    filters.team,
-    filters.position,
-    filters.offensiveRole,
-    filters.born,
-    allPlayers,
-    headers,
-  ]);
-  
+ 
   const sortData = (data) => {
     if (!filters.sortStat) return data;
 
