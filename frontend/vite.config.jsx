7f036ff24a9e37968d7d8aa5aadf49302d8697ea
Dennis <dennis.uhlig@icloud.com>
2024-10-07 23:48:11 +0200

    11

--- frontend/vite.config.jsx
+++ frontend/vite.config.jsx
@@ -1,15 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './',  // Das Root-Verzeichnis für den Build
-  publicDir: 'public', // Der Ordner, in dem sich public assets befinden
+  root: './',  // Stellt sicher, dass das Root-Verzeichnis korrekt ist
+  publicDir: 'public', // Das Verzeichnis, in dem sich public-assets befinden
   build: {
     outDir: 'dist',  // Der Ordner, in den der Build exportiert wird
     rollupOptions: {
-      input: './public/index.html',  // Der Einstiegspunkt ist die index.html im public-Ordner
+      input: './public/index.html',  // Stellt sicher, dass Vite die index.html aus public verwendet
     },
   },
 });
