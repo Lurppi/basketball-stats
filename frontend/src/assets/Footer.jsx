b3ef9416fbe53aa2ef510bd5cb3a1abc590b92b4
Dennis <dennis.uhlig@icloud.com>
2025-01-07 03:14:11 +0100

    11

--- frontend/src/assets/Footer.jsx
+++ frontend/src/assets/Footer.jsx
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <p>&copy; 2024 NBBL Stats Dashboard</p>
+      <p>&copy; 2025 NBBL Stats Dashboard</p>
       <p>
         <Link to="/impressum" className="footer-link">Impressum</Link> | {/* Trennzeichen hinzufügen */}
         <Link to="/glossary" className="footer-link">Glossary</Link> | {/* Trennzeichen hinzufügen */}
