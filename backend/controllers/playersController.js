24cfb05242a020ae3fc46a67c6247d34853108a4
Lurppi <dennis.uhlig@icloud.com>
2025-05-28 01:22:17 +0200

    11

--- backend/controllers/playersController.js
+++ backend/controllers/playersController.js
@@ -373,7 +373,7 @@ const getAllPlayers = (req, res) => {
 
       // Filtere nur Spieler mit mindestens X gespielten Minuten ("MP") und Saison-Daten
       if (
-        parseFloat(cleanedRow.MP) >= 50 &&
+        parseFloat(cleanedRow.MP) >= 200 &&
         cleanedRow.SEASON_TYPE.trim().toUpperCase() === 'SEASON'
       ) {
         results.push(cleanedRow);
